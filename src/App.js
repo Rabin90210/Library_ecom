47768e4cc0e35e1371a8f621344a5a1b70879027
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-06-19 09:40:32 +1000

    js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
 
-import './App.css';
 
 function App() {
   return (
