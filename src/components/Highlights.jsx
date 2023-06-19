c41e63b7cd9e0bb82e66eb39af665dbd3d2a4bf6
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-06-19 11:14:31 +1000

    ad

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,1 +1,14 @@
-ffc
+import React from 'react'
+
+const Highlights = () => {
+  return (
+    <section id="highlights">
+        <div className="container">
+            <div>
+
+            </div>
+        </div>
+
+    </section>
+  )
+}
